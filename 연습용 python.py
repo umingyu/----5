8f577b65512add01a1f794e7b53944b951eb8dea
shinmingyu <sinmingyu022@gmail.com>
2025-04-10 11:21:11 +0900

    modified:   "\354\227\260\354\212\265\354\232\251 python.py"

--- 연습용 python.py
+++ 연습용 python.py
@@ -1,6 +1,12 @@
-i,e=0,0
+i,e,r=0,0,""
 
-for i in range(2,10,1):
-    print("#%d단#."%i)
-    for e in range(1, 10, 1):
-        print("%d * %d = %d" % (i, e, i * e))
+for i in range(2,10):
+    r=r+(" # %d단 #"%i)
+
+print(r)
+
+for i in range(1, 10) : 
+    r=""
+    for e in range(2,10):
+        r=r+str("%2dx%2d=%2d"%(e, i, e*i))
+    print(r)
